0d979b8d973b4d8cfc8d5801dbe0c5e43ee9ff66
Vitor Izidoro <80259282+Vitor-Izidoro@users.noreply.github.com>
2026-06-09 13:40:33 -0300

    Add files via upload

---
+++ Tipos.hs
@@ -0,0 +1,35 @@
+module Tipos where
+
+import Data.Map (Map)
+import Data.Time.Clock (UTCTime)
+
+-- 1. Item: Registro básico do domínio
+data Item = Item {
+    itemID     :: String,
+    nome       :: String,
+    quantidade :: Int,
+    categoria  :: String
+} deriving (Show, Read, Eq)
+
+-- 2. Inventario: Estrutura de dados para armazenar os itens
+type Inventario = Map String Item
+
+-- 3. AcaoLog: Tipo de dado algébrico (ADT) para as ações
+data AcaoLog = Add 
+             | Remove 
+             | Update 
+             | QueryFail
+             deriving (Show, Read, Eq)
+
+-- 4. StatusLog: ADT para o resultado da operação
+data StatusLog = Sucesso 
+               | Falha String
+               deriving (Show, Read, Eq)
+
+-- 5. LogEntry: Registro completo para auditoria
+data LogEntry = LogEntry {
+    timestamp :: UTCTime,
+    acao      :: AcaoLog,
+    detalhes  :: String,
+    status    :: StatusLog
+} deriving (Show, Read, Eq)
